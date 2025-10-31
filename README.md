2d149d4fce8631d7bb95625aa35520d13080406c
stackxp <170874486+stackxp@users.noreply.github.com>
2025-10-31 23:39:39 +0100

    add stuff

---
+++ README.md
@@ -0,0 +1,23 @@
+# Copyparty grid video player
+
+Play any video in the copyparty grid view using just a single JavaScript and a video.
+
+## How to use
+
+1. Copy `badapple.js` and the video (as "badapple.mp4") into a folder on your copyparty server
+2. Click the π-symbol below the file view.
+3. Type `import("[path to where you are]/badapple.js")` and click OK
+4. Switch to grid view by pressing `g`.
+5. Click the π again and type `badapple()`.
+6. Zoom out by pressing `CTRL` and scrolling down.
+7. Press enter.
+8. Done! :thumbsup:
+
+> [!NOTE]
+> You can also use some arguments in the `badapple()` function, for example:
+> ```js
+> badapple({
+> 	video_path: "/videos/liar_dancer.mp4",
+> 	grid_text: "Liar dancer"
+> })
+> ```
---
+++ badapple.js
@@ -0,0 +1,71 @@
+"use strict"
+
+const pixel_size = "5rem"
+
+window.badapple = ({video_path = "./badapple.mp4", grid_text = "BAD APPLE!!"}) => {
+	let qs = (q, o=document) => o.querySelector(q)
+	let qsa = (q, o=document) => o.querySelectorAll(q)
+
+	let base = null
+	let canvas = new OffscreenCanvas(1, 1)
+	let ctx = canvas.getContext("2d")
+
+	let the_grid = qs("#ggrid")
+	if (qs("#gfiles").style.display == "none")
+		throw Error("Please make sure you have the grid view enabled!")
+
+	// Create a copy of the first grid item
+	let first_file = qs("a", the_grid)
+	first_file.href = video_path
+	first_file.firstChild.style.width = first_file.firstChild.style.height = pixel_size
+	first_file.firstChild.style.border = "1px solid transparent" // Disable empty image border
+	first_file.firstChild.src = ""
+	first_file.lastChild.innerHTML = grid_text
+	
+	// Determine grid size (cursed)
+	let grid_style = getComputedStyle(the_grid)
+	let item_rect = first_file.getBoundingClientRect()
+	canvas.width = (grid_style.gridTemplateColumns.match(/ /g) || []).length + 1
+	canvas.height = Math.round((window.innerHeight - item_rect.top) / item_rect.height)
+	let num_pixels = canvas.width * canvas.height
+	
+	base = first_file.cloneNode(true)
+	qsa("a", the_grid).forEach((e) => e.remove())
+
+	// Fill dat grid
+	let grid = []
+	for (let i = 0; i < num_pixels; i++) {
+		let n = base.cloneNode(true)
+		the_grid.appendChild(n)
+		grid.push(n.firstChild)
+	}
+	
+	let video = document.createElement("video")
+	video.onerror = () => {
+		throw Error("Video not found")
+	}
+	
+	// Updating the grid
+	let id = setInterval(() => {
+		ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+		let data = ctx.getImageData(0, 0, canvas.width, canvas.height).data
+		
+		for (let i = 0; i < num_pixels; i++) {
+			let values = data.slice(i * 4, (i + 1) * 4)
+			let string = new Array(...values).map((v) => v.toString(16).padStart(2, "0")).join("")
+			grid[i].style.background = "#" + string
+		}
+	}, 16)
+	
+	video.onended = () => clearInterval(id)
+	video.src = video_path
+	
+	setTimeout(() => qs("#modal-ok").onclick = () => {
+		// From util.js
+		modal.hide()
+		video.play()
+	}, 5)
+
+	// You propably can't even read this
+	return `Copyparty grid video player\n\nVideo path "${video_path}"\nGrid Size: ${canvas.width}x${canvas.height}\n\nPress OK to start.`
+}
